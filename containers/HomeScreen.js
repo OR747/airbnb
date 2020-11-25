8bcf4d438e7fdec6959dfff0bddf0e0d56689511
RHARRABT <rharrabt@MacBook-Pro-de-RHARRABT.local>
2020-11-25 17:51:13 +0100

    V2

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import { Button, Text, View, FlatList } from "react-native";
-
+import axios from "axios";
 export default function HomeScreen() {
   const navigation = useNavigation();
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  const Data = async () => {
+  const fetchData = async () => {
     try {
       const response = await axios.get(
-        " https://express-airbnb-api.herokuapp.com/rooms"
+        "https://express-airbnb-api.herokuapp.com/rooms"
       );
       console.log(response.data);
       setData(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error.message);
     }
   };
+  useEffect(() => {
+    console.log("Rentre dans le useEffect");
+    fetchData();
+  }, []);
 
-  return (
+  return isLoading ? (
+    <Text>en cour de chargement</Text>
+  ) : (
     <View>
       <View>
         <Text>Welcome home!</Text>
@@ -26,12 +35,18 @@ export default function HomeScreen() {
           onPress={() => {
             navigation.navigate("Profile", { userId: 123 });
           }}
+          keyExtractor={(item) => item.price}
         />
       </View>
       <FlatList
-        data={Data}
-        renderItem={({ photos }) => {
-          return <Text>{photos.url}</Text>;
+        data={data}
+        renderItem={({ item }) => {
+          console.log(item.photos.url);
+          return (
+            <Text>
+              {item.photos.url} {item.price} {item.title}
+            </Text>
+          );
         }}
       />
     </View>
